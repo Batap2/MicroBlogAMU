a6e76ac28d9785748e9d99c8de5ef52c909508c7
manon <pikamanon@gmail.com>
2022-02-25 16:21:22 +0100

    client publisher

--- src/ClientPublisher.java
+++ src/ClientPublisher.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
@@ -16,12 +19,29 @@ public class ClientPublisher {
         Socket socket = new Socket();
         InetSocketAddress inetSocketAddress = new InetSocketAddress(ip, port);
         socket.connect(inetSocketAddress);
+        BufferedReader inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner keyboard = new Scanner(System.in);
         System.out.println("Veuillez entrer votre nom d'utilisateur");
-        String pseudo = scanner.next();
+        String pseudo = keyboard.next();
+        System.out.println("Bienvenue @"+pseudo);
 
-        int bou = 1;
+        String entete = "PUBLISH author:@"+pseudo+"\r\n";
+
+        System.out.println("Vous pouvez désormais entrer vos messages");
+
+        while(keyboard.hasNext()){
+            String corps = keyboard.nextLine();
+            corps = corps + "\r\n";
+            String message = entete+""+corps;
+            byte[] msg = message.getBytes();
+
+            OutputStream outputStream = socket.getOutputStream();
+            outputStream.write(msg);
+
+            System.out.println(inputStream.readLine());
+        }
+
 
 
 
